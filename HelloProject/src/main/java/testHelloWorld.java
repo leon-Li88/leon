dd484cb79835e389fc637ccb0eb17582cf53b790
leon-Li88 <995215840@qq.com>
2019-08-20 19:58:22 +0800

    leon写的helloworld

---
+++ HelloProject/src/main/java/testHelloWorld.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class testHelloWorld{
+    @Test
+    public void test(){
+        System.out.println("HelloWorld");
+        System.out.println("leon写的代码");
+    }
+}
